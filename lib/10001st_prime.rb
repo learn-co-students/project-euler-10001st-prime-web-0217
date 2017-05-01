578af2972f5be1e9481ab0a9adc16e2466a91ede
github username <github email address>
2017-05-01 17:07:16 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def is_prime?(number)
+  ((2..(Math.sqrt(number)))).each do |i|
+    return false if number % i == 0
+  end
+  return true
+end
+
+def prime_number_for(nth_element)
+  count = 3
+  index = 1
+
+  while true
+    index += 1 if is_prime?(count)
+    return count if index == nth_element
+    count += 2
+  end
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+class Prime
+  attr_accessor :number, :nth_element
+  def initialize(nth_element)
+    @nth_element = nth_element
+    @number = prime_number_for
+  end
+
+  def is_prime?(number)
+    ((2..(Math.sqrt(number)))).each do |i|
+      return false if number % i == 0
+    end
+    return true
+  end
+
+  def prime_number_for
+    count = 3
+    index = 1
+    while true
+      index += 1 if is_prime?(count)
+      return count if index == @nth_element
+      count += 2
+    end
+  end
+
+end
