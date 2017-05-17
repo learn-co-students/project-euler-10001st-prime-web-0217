66e41d06b58ffad23a4ab0ba167bc3be8a0f04c1
hwalborn <hwalborn@gmail.com>
2017-05-16 20:03:16 -0400

    prime rib

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+  primes = []
+  i = 2
+  while(primes.length < num)
+    count = 0
+    (1..Math.sqrt(i).floor).each do |number|
+      if(i % number == 0)
+        count += 1
+      end
+    end
+    if(count == 1)
+      primes.push(i)
+    end
+    i += 1
+  end
+  primes.max
+end
