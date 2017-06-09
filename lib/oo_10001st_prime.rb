69e6c45fdd7080797972447a2e87c235f3ae1871
fakebenjay <fakebenjay@gmail.com>
2017-06-09 01:51:44 -0400

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,42 @@
-# Implement your object-oriented solution here!
+class Prime
+  attr_reader :primes
+
+  def initialize(nth_element)
+    @primes = []
+    counter = 2
+
+    until @primes.length == nth_element
+      if prime?(counter) == true
+        @primes << counter
+      end
+      counter += 1
+      puts counter
+    end
+    return @primes[-1]
+  end
+
+  def number
+    return @primes[-1]
+  end
+
+  private
+
+  def prime?(number)
+    if number <= 1 || (number.to_f != number.to_i)
+      return false
+    elsif number == 2 || number == 3
+      return true
+    else
+    	array = (2..Math.sqrt(number)).to_a
+
+      array.each do |divisor|
+        for divisor in (2...number)
+          if number.to_f % divisor.to_f == 0
+            return false
+          end
+        end
+    		return true
+      end
+    end
+  end
+end
